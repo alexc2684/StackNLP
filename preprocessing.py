bef6946268101d5cc4ea7229c49012e4148a0de9
Alex Chan <alex.chan@berkeley.edu>
2017-05-19 13:41:05 -0400

    Preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,31 @@
 import numpy as np
 import nltk
 import theano
+import csv
+import itertools
+
+VOCAB_SIZE = 10000
+UNKNOWN = "UNKNOWN_TOKEN"
+START = "SENTENCE_START"
+END = "SENTENCE_END"
+
+data = []
+with open("stack_overflow_comments.csv", "rt") as f:
+    reader = csv.reader(f)
+    data = itertools.chain(*[nltk.sent_tokenize(x[1].lower()) for x in reader])
+    sentences = ["%s %s %s" % (START, x, END) for x in data]
+
+sentences = sentences[2:]
+tokenized = [nltk.word_tokenize(sentence) for sentence in sentences]
+frequencies = nltk.FreqDist(itertools.chain(*tokenized))
+
+vocabulary = frequencies.most_common(VOCAB_SIZE-1)
+index_to_word = [word[0] for word in vocabulary]
+index_to_word.append(UNKNOWN)
+word_to_index = dict([(word, index) for word, index in enumerate(index_to_word)])
+
+for i, sent in enumerate(tokenized):
+    tokenized[i] = [word if word in word_to_index else UNKNOWN for word in sent]
+
+X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized])
+y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized])
